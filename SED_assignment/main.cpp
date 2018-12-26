2e2e1905cc10992d67feb49aa6b3665515564dd7
Yuumi Konishi <koni.overgang@gmail.com>
2018-12-26 13:02:46 +0900

    Initial Commit

---
+++ SED_assignment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SED_assignment
+//
+//  Created by Yuumi Konishi on 2018/12/26.
+//  Copyright © 2018 KoniKoni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
