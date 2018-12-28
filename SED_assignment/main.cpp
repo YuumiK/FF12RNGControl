0352b80b38549d886a57c607b7074271c48302d6
Yuumi Konishi <koni.overgang@gmail.com>
2018-12-28 21:23:40 +0900

    [add]別のテストケースを追加
    追加の結果も問題なし。

--- SED_assignment/main.cpp
+++ SED_assignment/main.cpp
@@ -8,10 +8,16 @@
 
 #include "MT.hpp"
 #include <iostream>
-int cure(unsigned long RNG)
+int magic = 99;
+int cure1(unsigned long RNG)
 {
     int lv = 66;
-    int magic = 99;
+    double result = 20 + (RNG % (int)(20 * 12.5)) / 100.0;
+    return (int)(result * (magic * (magic + lv)/256.0 + 2.0) * 1.5);
+}
+int cure2(unsigned long RNG)
+{
+    int lv = 99;
     double result = 20 + (RNG % (int)(20 * 12.5)) / 100.0;
     return (int)(result * (magic * (magic + lv)/256.0 + 2.0) * 1.5);
 }
@@ -24,12 +30,28 @@ int main(int argc, const char * argv[]) {
     MT mt;
     
     std::vector<int> curelist;
-    curelist.push_back(2047);
+    /*curelist.push_back(2047);
     curelist.push_back(2126);
-    curelist.push_back(2192);
+    curelist.push_back(2192);*/
     
-    std::cout << (mt.determinePosition(cure, curelist, 1) ? "found" : "not found") << std::endl;
+    curelist.push_back(2589);
+    curelist.push_back(2436);
+    curelist.push_back(2629);
+    
+    std::cout << (mt.determinePosition(cure2, curelist, 1) ? "found" : "not found") << std::endl;
     //mt.search(treasure, 1000);
     
     return 0;
 }
+/*
+ ケアル使用者のレベル:99
+ ケアル使用者の魔力:99
+ ケアルの値 1回目:2589
+ ケアルの値 2回目:2436
+ ケアルの値 3回目:2629
+ 現在位置(1-origin):7524
+ 現在位置再決定:0,レベル最大:1,レア盗み:2,レアトレジャー:3,POP等その他:4,チョコボ:5,終了:9
+ 4
+ トレジャーpop乱数(1スタート):7614
+ 動かす個数:90
+*/
